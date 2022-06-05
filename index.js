7fec2bc383e2c662ab2b07f9e47ca9f4cf5bd0f9
Luthfi Burhanuddin <luthfi.burhanuddin@gmail.com>
2022-06-05 22:15:20 +0800

    challenge-05

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (dataNames, functionSort) => {
+    sortData = functionSort(dataNames)
+
+    let result = []
+    for (let i = 0; i < sortData.length; i++) {
+      result.push(i + 1 + '. ' + sortData[i])
+    }
+
+  return result
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+// const sortAscending = null;
+const sortAscending = (dataNames) => {
+  let result = dataNames.sort()
+  return result
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+// const sortDescending = null;
+
+const sortDescending = (dataNames) => {
+  let result = dataNames.sort().reverse()
+  return result
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
